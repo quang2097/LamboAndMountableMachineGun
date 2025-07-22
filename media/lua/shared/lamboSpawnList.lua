3ff0bbe6dc65f858daca0c75c5a2d7fbd32e8637
quang2097 <usaltybroxd@gmail.com>
2025-07-22 13:21:29 +0700

    making template

---
+++ media/lua/shared/lamboSpawnList.lua
@@ -0,0 +1,13 @@
+if VehicleZoneDistribution then
+
+VehicleZoneDistribution.parkingstall.vehicles["Base.lambo"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.medium.vehicles["Base.lambo"] = {index = -1, spawnChance = 10};
+
+VehicleZoneDistribution.good.vehicles["Base.lambo"] = {index = -1, spawnChance = 10};
+
+VehicleZoneDistribution.trafficjamn.vehicles["Base.lambo"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.trafficjame.vehicles["Base.lambo"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.trafficjams.vehicles["Base.lambo"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.trafficjamw.vehicles["Base.lambo"] = {index = -1, spawnChance = 10};
+
+end
